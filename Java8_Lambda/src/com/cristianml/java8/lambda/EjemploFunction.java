a4049e642caaea03e7ac5742d80c350ebad83055
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-07-06 21:18:36 -0400

    Uso de expresion lambda BiFunction

--- Java8_Lambda/src/com/cristianml/java8/lambda/EjemploFunction.java
+++ Java8_Lambda/src/com/cristianml/java8/lambda/EjemploFunction.java
@@ -1,5 +1,6 @@
 package com.cristianml.java8.lambda;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class EjemploFunction {
@@ -16,10 +17,25 @@ public class EjemploFunction {
         String resultado = funcion1.apply("Cristian");
         System.out.println(resultado);
 
-        // Ejemplo con referencia de método
-        // podemos hacer esto cuando sólo se recibe un parámetro
+        // Ejemplo con referencia de método.
+        // podemos hacer esto cuando sólo se recibe un parámetro.
         Function<String, String> funcion2 = String::toUpperCase;
         System.out.println(funcion2.apply("mayúscula"));
+
+        // Ejemplo con BiFunction.
+        // es igual que el Function solo que recibe 2 parámetros.
+        BiFunction<String, String, String>  function3 = (a, b) -> a.toUpperCase().concat(" ").concat(b.toUpperCase());
+        String resultado2 = function3.apply("Monty", "Desho");
+        System.out.println(resultado2);
+
+        // Ejemplo cuando retorna un entero.
+        // El compareTo retorna un entero, si es 0 quiere decir que son iguales y si es un nro distinto es que no son iguales.
+        BiFunction<String, String, Integer> funcion4 = String::compareTo; // (a, b) -> a.compareTo(b);
+        System.out.println(funcion4.apply("holap", "hola"));
+
+        // Ejemplo con método concat
+        BiFunction<String, String, String> funcion5 = String::concat; // (a, b) -> a.concat(b);
+        System.out.println(funcion5.apply("Cristian", "Monty"));
     }
 
 }
