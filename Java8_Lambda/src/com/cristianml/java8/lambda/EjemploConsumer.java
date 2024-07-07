102171f20c496cf0a64dd1df17a3970a2bc8e5dc
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-07-06 21:16:45 -0400

    Metodos de referencia con Consumer y BiConsumer

--- Java8_Lambda/src/com/cristianml/java8/lambda/EjemploConsumer.java
+++ Java8_Lambda/src/com/cristianml/java8/lambda/EjemploConsumer.java
@@ -1,7 +1,11 @@
 package com.cristianml.java8.lambda;
 
+import com.cristianml.java8.lambda.models.UsuarioModel;
+
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
@@ -11,8 +15,9 @@ public class EjemploConsumer {
     // Es implementar un método sin necesidad de implementar una clase. Es una función anónima o método al vuelo.
     public static void main(String[] args) {
 
-        // Vamos a empezar con nuestra primera expresión lambda que será un consumer.
-        // Consumer es una expresión lambda que nos permite pasar por argumento un solo parámetro del tipo del genérico.
+        // Vamos a empezar con nuestra primera expresión lambda que será un Consumer.
+        // Consumer es una expresión lambda que nos permite pasar por argumento un solo parámetro del tipo del genérico,
+        // un consumer no devuelve nada, solo ejecuta un código de hace procedimiento.
         Consumer<Date> consumidor = fecha -> {
             SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
             System.out.println(f.format(fecha));
@@ -27,5 +32,31 @@ public class EjemploConsumer {
         };
 
         biConsumer.accept("Cristian", 25);
+
+        // Uso de referencia de método
+        Consumer<String> consumidor2 = System.out::println;
+        consumidor2.accept("Hello Everyone.");
+
+        // Estos consumer se utilizan mucho en el Api Stream de Java esta api usa mucho el Consumer, BiConsumer,
+        // Supplier, Predicate, Function, BiFunction,
+        // también en el forEach, ejemplo donde vamos a imprimir cada nombre de una lista con nuestra función lanbda:
+        List<String> nombres = Arrays.asList("Melsi", "Daniel", "Godoy", "Calebsin");
+        nombres.forEach(consumidor2);
+
+
+        // Ejemplo de un BiConsumer con método de referencia
+        UsuarioModel usuario = new UsuarioModel();
+        // Esto es normal
+        /*BiConsumer<UsuarioModel, String> asignarNombre = (persona, nombre) -> {
+            persona.setNombre(nombre);
+        };*/
+
+        // Con método de referencia
+        // Asumimos el primer parámetro como la clase principal la cual llama al método que tiene definido y asignamos
+        // el segundo argumento como parámetro para el método de la clase UsuarioModel.
+        BiConsumer<UsuarioModel, String> asignarNombre = UsuarioModel::setNombre;
+
+        asignarNombre.accept(usuario, "Diogo");
+        System.out.println("Nombre de usuario: " + usuario.getNombre());
     }
 }
---
+++ Java8_Lambda/src/com/cristianml/java8/lambda/models/UsuarioModel.java
@@ -0,0 +1,14 @@
+package com.cristianml.java8.lambda.models;
+
+public class UsuarioModel {
+
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
